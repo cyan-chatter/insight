29fc9f466652720e025764a4e04b3734acdf7c29
Sayan Chatterjee <0+radagast0@users.noreply.github.com>
2020-10-29 07:19:40 +0530

    teachers router Integrated to app

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(cookieParser())
 app.use(studentRouter)
-// app.use(teacherRouter)
+app.use(teacherRouter)
 
 //port value
 const port = process.env.PORT||3000
