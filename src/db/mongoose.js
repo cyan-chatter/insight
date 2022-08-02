8490181966a4a0f8a89635d41af0797a5bb89cec
Sayan Chatterjee <sayanchatterjee2000.com>
2022-08-03 02:52:26 +0530

    removed redundant vars

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,9 @@
-// C:/Users/DELL/mongo-4/mongodb/bin/mongod.exe --dbpath=C:/Users/DELL/mongo-4/mongodb-data
-
-const mongodb = require('mongodb')
 const mongoose = require('mongoose')
-
-//const MongoClient = mongodb.MongoClient
 const dbName = 'insight-base' 
 const mongoaddress =
   process.env.MONGODB_URI ||
   process.env.MONGODB_URL ||
   "mongodb://127.0.0.1:27017/insight-base";
-
 
 mongoose.connect(mongoaddress,{
     useNewUrlParser: true,
