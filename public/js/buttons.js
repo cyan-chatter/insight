f5f9c640b1518ffb53c52570a65de2c164ef2881
Sayan Chatterjee <sayanchatterjee2000.com>
2022-08-03 17:10:24 +0530

    fixed the outer pages

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -4,6 +4,7 @@ const buttons = document.getElementsByClassName("button")
 for(let btn of buttons){
     btn.addEventListener("click", (event) => {
       let go_to_href = event.target.getAttribute("href");
+      window.history.pushState({}, "", window.location);
       if (go_to_href && go_to_href !== "") window.location.replace(go_to_href);
     });
 }
